c2aa5c351312b45cfe1a8519bb6fc3af69577b4c
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2024-12-24 11:36:37 +0330

    start app

--- Kavenegar/Controllers/HomeController.cs
+++ Kavenegar/Controllers/HomeController.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Kavenegar.Controllers
+namespace KavenegarSample.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult SendTest()
+        {
+            ViewBag.Rslt = string.Empty;
+            try
+            {
+                Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(KavenegarRes.ApiKey);
+                var result = api.Send("2000500666", "09117449565", "خدمات پیام کوتاه کاوه نگار");
+                ViewBag.Rslt = result.Messageid.ToString();
+            }
+            catch (Kavenegar.Exceptions.ApiException ex)
+            {
+                // در صورتی که خروجی وب سرویس 200 نباشد این خطارخ می دهد.
+                ViewBag.Rslt = "Message : " + ex.Message;
+            }
+            catch (Kavenegar.Exceptions.HttpException ex)
+            {
+                // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
+                ViewBag.Rslt = "Message : " + ex.Message;
+            }
             return View();
         }
     }
