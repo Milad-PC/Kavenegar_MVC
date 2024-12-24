8dc474e316db53654616ecf0cb269068f6d2bd7a
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2024-12-24 13:13:32 +0330

    start test

--- Kavenegar/Controllers/HomeController.cs
+++ Kavenegar/Controllers/HomeController.cs
@@ -8,30 +8,30 @@ namespace KavenegarSample.Controllers
 {
     public class HomeController : Controller
     {
+        private Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(KavenegarRes.ApiKey);
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult SendTest()
+        public ActionResult Send()
         {
-            ViewBag.Rslt = string.Empty;
-            try
-            {
-                Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(KavenegarRes.ApiKey);
-                var result = api.Send("2000500666", "09117449565", "خدمات پیام کوتاه کاوه نگار");
-                ViewBag.Rslt = result.Messageid.ToString();
-            }
-            catch (Kavenegar.Exceptions.ApiException ex)
-            {
-                // در صورتی که خروجی وب سرویس 200 نباشد این خطارخ می دهد.
-                ViewBag.Rslt = "Message : " + ex.Message;
-            }
-            catch (Kavenegar.Exceptions.HttpException ex)
-            {
-                // در زمانی که مشکلی در برقرای ارتباط با وب سرویس وجود داشته باشد این خطا رخ می دهد
-                ViewBag.Rslt = "Message : " + ex.Message;
-            }
-            return View();
+            var rslt = api.Send(sender : KavenegarRes.Sender , receptor : "09117449565", message : "Hello Kavenegar");
+            return View(rslt);
+        }
+        public ActionResult SendArray()
+        {
+            //تعداد گیرنده با تعداد پیام باید برابر باشد
+            var rslt = api.SendArray(
+                senders : new List<string> { KavenegarRes.Sender , KavenegarRes.Sender , KavenegarRes.Sender } ,
+                receptors : new List<string> { "09117449565" , "09117449565" , "09117449565" },
+                messages : new List<string> { "Hello Kavenegar 1" , "Hello Kavenegar 2" , "Hello Kavenegar 3" }
+                );
+            return View("Send",rslt.First());
+        }
+        public ActionResult Status()
+        {
+            var rslt = api.Status("1895418878");
+            return View(rslt);
         }
     }
 }
